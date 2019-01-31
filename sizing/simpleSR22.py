18dc5b7163c81fe4b285436074ffd26cb2772b05
wmarrujo <willmarrujo@gmail.com>
2019-01-30 22:03:26 -0500

    close-ish sizing I guess

--- sizing/simpleSR22.py
+++ sizing/simpleSR22.py
@@ -18,10 +18,10 @@ TOlength = convert(1868, "ft", "m") # takeoff runway length
 LLength = convert(2345, "ft", "m") # landing runway length
 R = convert(700, "nmi", "m") # range at 8000ft 
 b = convert(38 + 1/3, "ft", "m") # wingspan
-S = convert(144.9, "ft^2", "m^2") # wing planform area
+S = convert(149.9, "ft^2", "m^2") # wing planform area
 We = convert(2253, "lb", "N") # empty weight
 W0 = convert(3600, "lb", "N") # gross weight
-AR = 9.0 # aspect ratio
+AR = 9.8 # aspect ratio
 n = 2700*2*pi # cruise RPM
 seats = 5 # seats (including pilot)
 fuelCapacity = convert(92, "gal", "m^3") # fuel capacity
@@ -40,20 +40,27 @@ Vcruise = convert(183, "kts", "m/s") # cruise speed
 Vloiter = convert(120, "kts", "m/s") # speed during loiter
 
 # ASSUMED PARAMETERS
-ηpcruise = 0.85 # cruise efficiency
-ηploiter = 0.85 # loiter efficiency
+ηpcruise = 0.9 # cruise propeller efficiency
+ηploiter = 0.9 # loiter propeller efficiency
 Cbhpcruise = convert(0.4, "lb/hr*hp", "kg/J") # Brake specific fuel consumption during cruise
 Cbhploiter = convert(0.5, "lb/hr*hp", "kg/J") # Brake specific fuel consumption during loiter
 CD0 = 0.02 # Parasite Drag Coefficient
 e = 0.8 # oswald efficiency factor
+KLD = 11
+Awetted = 1.6
 def EmptyWeightFraction(W0): # Empty Weight Fraction
-    return 1.51e-5 * convert(W0, "N", "lb") + 0.637 # trendline from composite airplanes from airplane database
+    #return 1.51e-5 * convert(W0, "N", "lb") + 0.637 # trendline from composite airplanes from airplane database
+    #return 1.51*convert(W0, "N", "lb")**(-1) # raymer
+    return 0.911*convert(W0, "N", "lb")**(-0.053) # nicolai
+    #return 0.688*convert(W0, "N", "lb")**(-9.23e-3) # 2nd trendline
+    #return 1.69*convert(W0, "N", "lb")**(-0.124) # 3rd trendline
 
 ################################################################################
 # SIZING
 ################################################################################
 
-LDmax = 1/2 * 1/sqrt(CD0 / (pi * AR * e)) # L/D
+# LDmax = 1/2 * 1/sqrt(CD0 / (pi * AR * e)) # maximum L/D
+LDmax = KLD * sqrt(Awetted)
 print(LDmax)
 Ecruise = R / Vcruise # cruise time
 WeightFraction23 = exp(-(Ecruise * Vcruise * Cbhpcruise) / (ηpcruise * LDmax)) # cruise-climb
@@ -64,14 +71,15 @@ FuelWeightFraction = 1.01 * (1 - MissionWeightFraction)
 W0guess = convert(2300, "lb", "N")
 iteration = 0
 while 1 < abs(W0 - W0guess) and iteration < 100:
-    print("W0guess = {0:.0f}".format(convert(W0guess, "N", "lb")))
-    W0guess = W0
+    #print("W0guess = {0:.0f}".format(convert(W0guess, "N", "lb")))
+    W0 = W0guess
     
-    We = EmptyWeightFraction(W0guess) * W0guess
-    Wf = W0 * FuelWeightFraction # mission fuel weight
-    W0 = We + Wf + Wpay # gross weight
+    We = EmptyWeightFraction(W0) * W0guess
+    Wf = W0guess * FuelWeightFraction # mission fuel weight
+    W0guess = We + Wf + Wpay # gross weight
     
     iteration += 1
+W0 = W0guess
 
 ################################################################################
 # OUTPUT

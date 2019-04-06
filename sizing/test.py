000bece63da4357229c15e91c07dffa05a3599e6
eiglehar <47483316+eiglehar@users.noreply.github.com>
2019-04-06 15:22:11 -0400

    formatted empty weight inputs a bit in test.py

--- sizing/test.py
+++ sizing/test.py
@@ -32,23 +32,33 @@ powerplant.percentElectric = 0
 powerplant.generatorOn = False
 airplane.powerplant = powerplant
 
+############### MAIN WING INPUT AREA ###############
 airfoil = Airfoil(os.path.join(sys.path[0], "data", "SF1.csv"))
 wing = Wing(1, convert(40*5, "ft^2", "m^2"), 0.02, convert(40, "ft", "m"), 0, 1, airplane)
 wing.maximumLiftCoefficient = 2
 wing.airfoil = airfoil
 airplane.wing = wing
 
+############### FUSELAGE INPUT AREA ###############
 fuselage = Fuselage(1, convert(7, "ft", "m"), convert(30, "ft", "m"), airplane)
 airplane.fuselage = fuselage
 
+############### STABILIZERS INPUT AREA ###############
 horizontalStabilizer = HorizontalStabilizer(1.2, convert(10*3, "ft^2", "m^2"), 0.12, convert(10, "ft", "m"), 0, 1, airplane)
 verticalStabilizer = VerticalStabilizer(1.1, convert(6*3, "ft^2", "m^2"), 0.12, convert(6, "ft", "m"), 0, 1, airplane)
+tail = Tail()
+tail.horizontalStabilizer = horizontalStabilizer
+tail.verticalStabilizer = verticalStabilizer
+airplane.tail = tail
 
 
+############### PROPELLER INPUT AREA ###############
 propeller = Propeller()
 propeller.diameter = convert(6, "ft", "m")
 propeller.angularVelocity = 0
 propeller.efficiency = 0.9
+
+############### ENGINE INPUT AREA ###############
 engineNacelle = Nacelle(1, convert(1.5, "ft", "m"), convert(4, "ft", "m"))
 engine = Engine()
 engine.maxPower = convert(130, "hp", "W")
@@ -57,18 +67,15 @@ engine.nacelle = engineNacelle
 engine.mass = 98 # kg
 engineL = engine
 engineR = copy.deepcopy(engine)
-tail = Tail()
-tail.horizontalStabilizer = horizontalStabilizer
-tail.verticalStabilizer = verticalStabilizer
-airplane.tail = tail
+airplane.engines = [engineL, engineR] # [engine object] # list of engines on airplane
+installedEngine = InstalledEngine(airplane)
 
+############### LANDING GEAR INPUT AREA ###############
 NLand = airplane.LoadFactor * 1.5 # Ultimate Load Factor
 mainGear = MainGear(NLand, convert(1, "m", "ft"), airplane)# First input = LandingLoadFactor, Second input = lengthMainGear (m)
 frontGear = FrontGear(NLand, convert(1, "m", "ft"), airplane) # First input = LandingLoadFactor, Second input = lengthFrontGear (m)
 
-airplane.engines = [engineL, engineR] # [engine object] # list of engines on airplane
-installedEngine = InstalledEngine(airplane)
-
+############### MISCELLANEOUS COMPONENT INPUT AREA ###############
 fuelSystem = FuelSystem(airplane)
 airplane.fuelSystem = fuelSystem
 flightControls = FlightControls(airplane)
@@ -79,21 +86,20 @@ avionics = Avionics(4000) # INPUT = uninstalled avioncs weight [N] (typically 80
 airplane.avionics = avionics
 electronics = Electronics(airplane)
 airplane.electronics = electronics
-
 airplane.pilots = 1
 airplane.passengers = 3
 airconIce = AirConIce(airplane) # Airconditioning and Anti Ice
 airplane.airconIce = airconIce
-
 furnishings = Furnishings(airplane)
 airplane.furnishings = furnishings
 
 airplane.components = [wing, engineL.nacelle, engineR.nacelle, fuselage, horizontalStabilizer, verticalStabilizer, installedEngine, mainGear, frontGear, fuelSystem, hydraulics, flightControls, avionics, electronics, airconIce, furnishings] # [component objects] # list of components making up airplane (including parts used elsewhere)
+
 airplane.oswaldEfficiencyFactor = 0.8
 airplane.compressibilityDragCoefficient = 0
 airplane.miscellaneousParasiteDragFactor = 0.004 # FIXME: ?
 airplane.emptyWeight = sum([component.mass for component in airplane.components]) # TODO: will be replaced with component weight buildup
-print(airplane.emptyWeight)
+
 airplane.altitude = 0
 airplane.position = 0
 airplane.speed = 0

7248fe91b710974e762147fb1e1f75aeed7ad6fd
wmarrujo <willmarrujo@gmail.com>
2019-04-13 17:58:08 -0400

    actually having defining parameters define the airplane

--- test.py
+++ test.py
@@ -24,12 +24,35 @@ from testcraft import defineAirplane as defineTestcraft
 from matplotlib.pyplot import *
 
 ################################################################################
-# DEFINING AN AIRPLANE
+# TESTS
 ################################################################################
 
-DPS = {
-    "wing loading": convert(50, "lb/ft^2", "N/m^2"),
-    "power to weight ratio": convert(0.072, "hp/lb", "W/N")}
+WS = convert(50, "lb/ft^2", "N/m^2")
+PW = convert(0.072, "hp/lb", "W/N")
 
-PPs = getPerformanceParameters("tecnam", DPS, designMission, silent=True)
-print(PPs)
+# Driving parameters
+# WSs = [WS * 0.8, WS, WS * 1.2]
+# PWs = [PW * 0.8, PW, PW * 1.2]
+WSs = [WS * 0.8]
+PWs = [PW * 0.8]
+
+# FOR loop that iterates through 3x3 permutations of W/S and T/W and create matrix
+
+ids = [[airplaneDefinitionID("testcraft", {
+    "wing loading": WS,
+    "power to weight ratio": PW
+    }) for WS in WSs] for PW in PWs]
+
+initialAirplanes = [[loadInitialAirplane(id) for id in row] for row in ids]
+simulations = [[loadSimulation(id) for id in row] for row in ids]
+finalAirplanes = [[loadFinalAirplane(id) for id in row] for row in ids]
+
+sim = simulations[0][0]
+
+ps = sim["position"]
+hs = sim["altitude"]
+
+figure()
+plot([convert(p, "m", "nmi") for p in ps], [convert(h, "m", "ft") for h in hs])
+
+show()
